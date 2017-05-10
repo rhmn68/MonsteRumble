44c5f3d01c96760c82b3db1687dd69ccf5a3e11e
Rahman <auliar68@gmail.com>
2017-05-10 14:09:19 +0700

    Try

--- src/WarList.java
+++ src/WarList.java
@@ -9,21 +9,39 @@ public class WarList {
     ArrayList<Weapon> dbWeapon = new ArrayList<>();
     ArrayList<Armor> dbArmor = new ArrayList<>();
 
-    public void addMonster(Monster m){
+    public void getMonster(Monster m){
         dbMonster.add(m);
     }
-    public void addWeapon(Weapon w){
+    public void getWeapon(Weapon w){
         dbWeapon.add(w);
     }
-    public void addArmor(Armor a){
+    public void getArmor(Armor a){
         dbArmor.add(a);
     }
 
-    public Monster cariMonster(int idM){
+    public Monster setMonster(int idM){
        return dbMonster.get(idM);
     }
 
-    public Weapon cariWeapon(int idW){
+    public Weapon setWeapon(int idW){
         return dbWeapon.get(idW);
+    }
+
+    public Armor setArmor(int idA){
+        return dbArmor.get(idA);
+    }
+
+    public void addMonster(){
+        Monster m1 = new Monster(1,"Badak",75,10,20);getMonster(m1);
+        Monster m2 = new Monster(2,"Piton",100,20,0);getMonster(m2);
+        Monster m3 = new Monster(3,"Kura",125,5,10);getMonster(m2);
+    }
+
+    public void addWeapon(){
+        Weapon w1 = new Weapon();
+    }
+
+    public void addArmor(){
+
     }
 }
